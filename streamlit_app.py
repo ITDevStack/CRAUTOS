491aa096e2cfb176fb0075dcdfac3dea57c87f02
ITDevStack <163896368+ITDevStack@users.noreply.github.com>
2024-06-16 17:13:00 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -798,7 +798,7 @@ def estadisticas_visuales(cars_historico):
                 hide_index=True,
             )
 
-            st.wirte("hola")
+            st.write("hola")
         
         except:
             st.write("No existen potenciales inversiones")
