002bcd6b717cd353c41d6c69689739f0354807cd
ITDevStack <163896368+ITDevStack@users.noreply.github.com>
2024-06-16 17:14:12 -0600

    a

--- streamlit_app.py
+++ streamlit_app.py
@@ -798,7 +798,7 @@ def estadisticas_visuales(cars_historico):
                 hide_index=True,
             )
 
-            st.write("hola")
+            st.write(sum(modelo['Precio']))
         
         except:
             st.write("No existen potenciales inversiones")
